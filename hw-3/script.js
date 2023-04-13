f91202c9ecfa8559dc413fa0e3fec9d38b05afd4
ArcherOleg <chernovolezhek@yandex.ru>
2023-04-13 21:19:44 +0300

    added guidance and fixed my mistakes in the mobile version

--- hw-3/script.js
+++ hw-3/script.js
@@ -1,86 +1,86 @@
-// Задание 1
+// // Задание 1
 
-let password = 1234;
+// let password = 1234;
 
-let user = prompt(`Введите пароль`);
+// let user = prompt(`Введите пароль`);
 
-if (password == user) {
-    message = (`Пароль введен верно`);
-} else {
-    message = (`Пароль введен неправильно`);  
-}
+// if (password == user) {
+//     message = (`Пароль введен верно`);
+// } else {
+//     message = (`Пароль введен неправильно`);  
+// }
 
-alert (message);
+// alert (message);
 
-// Задание 2
+// // Задание 2
 
-let c = -3
+// let c = -3
 
-if (c >= 0 && c <= 10) {
-    alert (`верно`);
+// if (c >= 0 && c <= 10) {
+//     alert (`верно`);
 
-} else {
-    alert (`не верно`);  
-}
+// } else {
+//     alert (`не верно`);  
+// }
 
-// Задание 3
+// // Задание 3
 
-let d = 111;
-let e = 11;
-(d>100 || e>100)? alert(`Верно`) : alert(`Не верно`)
+// let d = 111;
+// let e = 11;
+// (d>100 || e>100)? alert(`Верно`) : alert(`Не верно`)
 
 
-// Задание 4
+// // Задание 4
 
-let a = 2;
+// let a = 2;
 
-let b = 3;
+// let b = 3;
 
-alert(a + b);
+// alert(a + b);
 
-// Задание 5
+// // Задание 5
 
-let monthNumber = Number (prompt(`Введите номер месяца, чтоб узнать время года`))
+// let monthNumber = Number (prompt(`Введите номер месяца, чтоб узнать время года`))
 
-switch (monthNumber) {
-    case 1:
-        alert(`Зима`);
-        break;
-    case 2:
-        alert(`Зима`);
-        break;
-    case 3:
-        alert(`Весна`);
-        break;
-    case 4:
-        alert(`Весна`);
-        break;
-    case 5:
-        alert(`Весна`);
-        break;
-    case 6:
-        alert(`Лето`);
-        break;
-    case 7:
-        alert(`Лето`);
-        break;
-    case 8:
-        alert(`Лето`);
-        break;
-    case 9:
-        alert(`Осень`);
-        break;
-    case 10:
-        alert(`Осень`);
-        break;
-    case 11:
-        alert(`Осень`);
-        break;
-    case 12:
-        alert(`Зима`);
-        break;
-    defult:
-        alert(`Введенно не верное значение`)
-        break;
+// switch (monthNumber) {
+//     case 1:
+//         alert(`Зима`);
+//         break;
+//     case 2:
+//         alert(`Зима`);
+//         break;
+//     case 3:
+//         alert(`Весна`);
+//         break;
+//     case 4:
+//         alert(`Весна`);
+//         break;
+//     case 5:
+//         alert(`Весна`);
+//         break;
+//     case 6:
+//         alert(`Лето`);
+//         break;
+//     case 7:
+//         alert(`Лето`);
+//         break;
+//     case 8:
+//         alert(`Лето`);
+//         break;
+//     case 9:
+//         alert(`Осень`);
+//         break;
+//     case 10:
+//         alert(`Осень`);
+//         break;
+//     case 11:
+//         alert(`Осень`);
+//         break;
+//     case 12:
+//         alert(`Зима`);
+//         break;
+//     defult:
+//         alert(`Введенно не верное значение`)
+//         break;
 
-}
+// }
