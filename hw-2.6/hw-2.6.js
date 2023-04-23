86cd502b876474c595df005c68d48b7aedb0e5e0
ArcherOleg <chernovolezhek@yandex.ru>
2023-04-23 15:19:51 +0300

    с 1го по 11 задания

--- hw-2.6/hw-2.6.js
+++ hw-2.6/hw-2.6.js
@@ -3,7 +3,7 @@
 const number = [1, 5, 4, 10, 0, 3];
 
 for (let i = 0; i < number.length; i++) {
-    if (number[i] == 10) break;
+    if (number[i] == 0)break;
     console.log(number[i]);
 }
 
@@ -66,14 +66,43 @@ console.log(arrNumber);
 
 //Задание 7
 
-let Numbers = [9, 8, 7, 6, 5];
+const arr = [9, 8, 7, 6, 5];
 
-let userNumber = prompt('Введите число 1 от 10');
+let userNumber = Number(prompt('Введите число 1 от 10'));
 
-
+if(arr.includes(userNumber)){
+    alert('Данное число входит в массив');
+} else if(isNaN(userNumber)){
+    alert('Вы ввели не число');
+} else {
+    alert('Данное число не входит в массив');
+}
 
 //Задание 8
 
 let word = 'abcdef';
 
 console.log([...word].reverse().join(''));
+
+//Задание 9
+
+let array = [[1, 2, 3,],[4, 5, 6]];
+
+console.log(array.flat());
+
+//Задание 10
+
+const arr = [1, 2, 3, 4, 5];
+
+for(let i = 0; i < arr.length - 1; i++){
+    console.log(arr[i] + arr[i + 1]);
+}
+
+//Задание 11
+
+const array = [2, 5, 3, 4, 7];
+
+const square = elem.map(el => el ** 2);
+
+console.log(square);
+
